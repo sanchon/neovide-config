808486d7b300aaca501854c2a725f70b3abdbd94
Hugo Sanchón <sanchon@gmail.com>
2026-03-10 23:40:40 +0100

    simpler

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,34 +1,17 @@
+-- Solo completado en búsqueda (/ y ?). Para comandos (:) se usa lo nativo:
+-- wildmenu, wildmode y wildoptions (ver init.lua).
 return {
   {
     "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-    },
+    dependencies = { "hrsh7th/cmp-buffer" },
     event = "CmdlineEnter",
     config = function()
       local cmp = require("cmp")
-
-      -- Cmdline : (comandos + rutas)
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline" },
-        }),
-      })
-
-      -- Búsqueda / y ? (usa el buffer actual)
       cmp.setup.cmdline({ "/", "?" }, {
         mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
+        sources = { { name = "buffer" } },
       })
     end,
   },
-  -- Fuente buffer requerida para / y ?
-  { "hrsh7th/cmp-buffer", event = "CmdlineEnter" },
 }
 
