6eea3f5350763c3254209dcae38f11ebedfc24a5
Hugo Sanchón <sanchon@gmail.com>
2026-03-11 16:07:34 +0100

    cambios de 2026-03-11

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,11 +1,13 @@
 return {
   "vimwiki/vimwiki",
-  config = function()
-    vim.g.vimwiki_list = {{
-      path = vim.fn.expand("~\\notas"),
-      syntax = "markdown",
-      ext = ".md",
-    }}
+  init = function()
+    vim.g.vimwiki_list = {
+      {
+        path = vim.fn.expand("~\\notas"),
+        syntax = "markdown",
+        ext = ".md",
+      },
+    }
     vim.g.vimwiki_global_ext = 0
     vim.g.vimwiki_markdown_link_ext = 1
     vim.g.vimwiki_url_maxsave = 0
