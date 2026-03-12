7067622ec50ee0336a4df7d6f8a89b9a1fd89278
Hugo Sanchón <sanchon@gmail.com>
2026-03-12 11:03:18 +0100

    fixed

--- lua/plugins/airline.lua
+++ lua/plugins/airline.lua
@@ -1,9 +1,18 @@
 return {
   "vim-airline/vim-airline",
-  config = function()
+  dependencies = {
+    "vim-airline/vim-airline-themes", -- Opcional, pero muy recomendado para cambiar estilos
+  },
+  init = function()
+    -- Habilita el uso de fuentes Powerline
     vim.g.airline_powerline_fonts = 1
+
+    -- (Opcional) Aquí puedes definir el tema que prefieras
+    -- vim.g.airline_theme = 'dark'
+    
+    -- (Opcional) Mostrar los buffers abiertos en la parte superior
+    vim.g['airline#extensions#tabline#enabled'] = 1
     vim.g["airline#extensions#branch#enabled"] = 1
-    vim.g["airline#extensions#tabline#enabled"] = 1
   end,
 }
 
